aa8723ddbed66a672ae834caf412fba3e1281e18
Kanwar Sahdra <ksingh7265@gmail.com>
2018-11-17 14:08:25 -0500

    readme update && fleshing out algorithm for converting to base64

--- set1/q1.rs
+++ set1/q1.rs
@@ -1,30 +1,39 @@
 // use std::io;
-
+// use std::iter::Iterator;
+// use std::collections::BitVec;
 
 fn hex_to_base64(hex_string: String) -> String {
-    // 1. convert string into hex, represented as bytes
-        // instructions say, operate on raw bytes
-        // assuming way to represent raw bytes is u8 -- 8 bits in a byte
-            // so, need to convert string into u8 that represents the hex
-        // each hex char is 4 bits 
-        // to operate on each character, can use a for loop on the char of the array (the string)
-        // &hex_string because it's a slice of the array   
+    /* 1. convert string into hex, represented as bytes */
 
-    // let mut bytes: &[u8]; // no, because these are fixed length ... could be fine, just allocate length of string 
-    // let mut v: Vec<u8> = Vec::new();
-
-    // for character in &hex_string {
-    //     v.push(character.as_bytes()); 
-    // }
-
-    // convert string into byte vector 
+    // convert hex string into byte vector 
     // https://doc.rust-lang.org/std/string/struct.String.html#method.into_bytes
-    let bytes = hex_string.into_bytes()
+    let bytes = hex_string.into_bytes();
 
-    // 2. convert bytes into base64
-        // each base64 char is 6 bits 
-        // for every 3 bytes, we have four 6 bit base64 characters (LCM(6,4) == 24)
+    /* 2. convert bytes into base64 */
 
+    // get our vector of bytes as bits now
+    // https://doc.rust-lang.org/1.2.0/std/collections/struct.BitVec.html#method.from_bytes
+    let bv = BitVec::from_bytes(&bytes);
+
+    // create the string output ahead of time, will be building it throughout
+    let mut result = String::new();
+
+    // each base64 char is 6 bits, each hex char is 4 bits
+    // for every 6 bytes, we have four 6 bit base64 characters (LCM(6,4) == 24) 
+
+    // this pattern below - a loop, and within it, a tuple of .next() iterator objects to collect values
+    // and then matching against those tuple items
+    // is from https://github.com/jakerr/cryptopals-rust/blob/master/src/conversions.rs 
+    // it's interesting - clumsy given the need for 'break's and matching all, but interesting way to
+    // get to MVP soln by explicitly enumerating the cases in a match statement
+    // then, can come back later or as writing the expressions corresponding to each case
+    // and notice the patterns, then rewriting the match condition, etc., finding a new structure
+    // for the algo
+    loop {
+        set = ( bv.next(), bv.next(),  )
+
+        
+    }
 
 
 
@@ -37,3 +46,8 @@ fn main(hex_string: String, base64_string: String) {
     if is_correct { print!("It works! Succesfully ...") } else { print!("Oops! Not quite there yet – think through it ...") };
 
 }
+
+
+// excellent, succinct explanation of the task: https://www.quora.com/How-does-base64-encoding-work/answer/Ke-Pi 
+// the answer above it gives a decent walk through of actually doing the conversion by hand
+// which is worthwhile
