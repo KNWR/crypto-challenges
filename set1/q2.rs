0a5bd27c0295ac9a367e536072c998a92453f5e7
Kanwar Sahdra <ksingh7265@gmail.com>
2018-11-17 23:54:23 -0500

    TIL: buffer is aka collection of bytes, very nice

---
+++ set1/q2.rs
@@ -0,0 +1,16 @@
+// TIL: buffer is just an array of bytes
+// is it a reference to, or can it act on?
+// i.e. necessarily &[u8], or can be [u8] ?
+fn fixed_xor(buffer1: type, buffer2: type) {
+    // not sure if len is right?
+    // also, since buffer - any datatype?
+    // so <T> as type?
+    assert_eq!(buffer1.len(), buffer2.len()); 
+    
+
+
+}
+
+fn main() {
+    fixed_xor(b1, b2);
+}
