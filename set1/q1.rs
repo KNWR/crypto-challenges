44fff8bde456ded75480722fe0a773e1a15e4939
Kanwar Sahdra <ksingh7265@gmail.com>
2018-11-17 20:58:11 -0500

    intuition behind stepping through bit by bit - realizing should do it byte by bte

--- set1/q1.rs
+++ set1/q1.rs
@@ -1,6 +1,6 @@
 // use std::io;
 // use std::iter::Iterator;
-// use std::collections::BitVec;
+use std::collections::BitVec;
 
 fn hex_to_base64(hex_string: String) -> String {
     /* 1. convert string into hex, represented as bytes */
@@ -13,31 +13,49 @@ fn hex_to_base64(hex_string: String) -> String {
 
     // get our vector of bytes as bits now
     // https://doc.rust-lang.org/1.2.0/std/collections/struct.BitVec.html#method.from_bytes
-    let bv = BitVec::from_bytes(&bytes);
+    let mut bv = BitVec::from_bytes(&bytes);
 
-    // create the string output ahead of time, will be building it throughout
-    let mut result = String::new();
+    // create the output ahead of time, will be building it throughout
+    let mut result = Vec::new();
 
     // each base64 char is 6 bits, each hex char is 4 bits
     // for every 6 bytes, we have four 6 bit base64 characters (LCM(6,4) == 24) 
 
-    // this pattern below - a loop, and within it, a tuple of .next() iterator objects to collect values
-    // and then matching against those tuple items
-    // is from https://github.com/jakerr/cryptopals-rust/blob/master/src/conversions.rs 
-    // it's interesting - clumsy given the need for 'break's and matching all, but interesting way to
-    // get to MVP soln by explicitly enumerating the cases in a match statement
-    // then, can come back later or as writing the expressions corresponding to each case
-    // and notice the patterns, then rewriting the match condition, etc., finding a new structure
-    // for the algo
-    loop {
-        set = ( bv.next(), bv.next(),  )
+    // why not just programmatically create this with a range?
+    // This is faster, more quickly accessed: https://stackoverflow.com/a/19061862 
+    // Using a match statement with a _ => fail! statement likely more secure / 
+    // cleaner code, see https://github.com/garrettr/cryptopals/blob/master/1-1-base64.rs 
+    const BASE_64: [char; 64] = [
+        'A','B','C','D','E','F','G','H',
+        'I','J','K','L','M','N','O','P',
+        'Q','R','S','T','U','V','W','X',
+        'Y','Z','a','b','c','d','e','f',
+        'g','h','i','j','k','l','m','n',
+        'o','p','q','r','s','t','u','v',
+        'w','x','y','z','0','1','2','3',
+        '4','5','6','7','8','9','+','/'
+    ];
 
-        
+    // add extra zeros to pad
+    let padding_amt = 24 - bv.len() % 24;  // or 3?
+    for _ in range(padding_amt) {
+        bv.push(0)
+    }
+
+    // iterating through the entire bit vector
+    for sixbit_pos in range_step(0, bv.len(), 24) {
+        for _ in range(0, 4) {
+            let mut sixbits : u32;
+            mut = 
+            result.push(BASE_64[ sixbits ])
+        }
     }
 
 
-
     // 3. output base64 -> string
+    print!("", &result);
+
+    result
 }
 
 fn main(hex_string: String, base64_string: String) {
@@ -51,3 +69,12 @@ fn main(hex_string: String, base64_string: String) {
 // excellent, succinct explanation of the task: https://www.quora.com/How-does-base64-encoding-work/answer/Ke-Pi 
 // the answer above it gives a decent walk through of actually doing the conversion by hand
 // which is worthwhile
+
+
+// note to self: note the pattern from https://github.com/jakerr/cryptopals-rust/blob/master/src/conversions.rs 
+// - a loop, and within it, a tuple of .next() iterator objects to collect values and 
+// then matching against those tuple items. interesting - clumsy given the need for 
+// 'break's and matching all, but interesting way to get to MVP soln by explicitly 
+// enumerating the cases in a match statement then, can come back later or as 
+// writing the expressions corresponding to each case and notice the patterns, then 
+// rewriting the match condition, etc., finding a new structure for the algo
